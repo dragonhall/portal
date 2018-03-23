ad06f58cf0b408d347deecb4188cdb6179241a88
Gabor Garami <hron@hron.me>
2018-03-23 18:24:41 +0100

    Users <-> Posts

--- app/models/fusion/post.rb
+++ app/models/fusion/post.rb
@@ -8,4 +8,15 @@ class Fusion::Post < ApplicationRecord
 
   belongs_to :forum, class_name: 'Fusion::Forum'
   belongs_to :thread, class_name: 'Fusion::Thread'
+  belongs_to :author_user, class_name: 'Fusion::User', foreign_key: 'post_author'
+  belongs_to :editor, class_name: 'Fusion::User', foreign_key: 'post_edituser'
+
+
+  def last_edit
+    {
+      user: self.editor,
+      time: self.post_edittime,
+      reason: self.reason_edit
+    }
+  end
 end
